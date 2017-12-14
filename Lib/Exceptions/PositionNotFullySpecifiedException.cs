6f6f02128efb10eab5b8bd14b86c81859f2592a0
Vlad <MrRefactoring@yandex.ru>
2017-12-14 21:48:53 +0300

    Add code. Stable Version

---
+++ Lib/Exceptions/PositionNotFullySpecifiedException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lib.Exceptions
+{
+    class PositionNotFullySpecifiedException: ApplicationException
+    {
+
+        public PositionNotFullySpecifiedException(): base() { }
+
+        public PositionNotFullySpecifiedException(string message) : base(message) { }
+
+    }
+}
---
+++ PingPong/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace PingPong
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void loadGame(object sender, RoutedEventArgs e)
+        {
+            new PlayGround.PlayGround().Show();  // Загужаем игровое поле
+            this.Close();  // Закрываем текущее окно
+        }
+
+    }
+}
